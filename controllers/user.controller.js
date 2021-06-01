805ff452261b0e9500ff17708cc8ff7db334d767
Puneet Singh <99singhpuneet99@gmail.com>
2021-06-02 02:57:19 +0530

    Feat - Add, update, delete existing User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/user.model");
 const { successResponse, errorResponse } = require("../utils");
+const { extend } = require("lodash");
 
 const getAllUsers = async (req, res) => {
   try {
@@ -39,4 +40,61 @@ const deleteAllUser = async (req, res) => {
   }
 };
 
-module.exports = { addUser, getAllUsers, deleteAllUser };
+const userIdCheck = async (req, res, next, id) => {
+  try {
+    const user = await User.findOne({ _id: id }).select("-__v");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    req.id = id;
+    req.user = user;
+    next();
+  } catch (error) {
+    return errorResponse(res, "User not found", error);
+  }
+};
+
+const getUser = async (req, res) => {
+  const { user } = req;
+  return successResponse(res, { message: "User retrieved successfully", user });
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const updateData = req.body;
+    let userToBeUpdated = await User.findOne({ _id: req.id });
+    userToBeUpdated = extend(userToBeUpdated, updateData);
+    const updatedUser = await userToBeUpdated.save();
+    updatedUser.__v = undefined;
+
+    return successResponse(res, {
+      message: "User updated successfully",
+      updatedUser,
+    });
+  } catch (error) {
+    return errorResponse(res, "Could not update the user", error);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.deleteOne({ _id: req.id });
+    return successResponse(res, {
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    return errorResponse(res, "Could not delete the user", error);
+  }
+};
+
+module.exports = {
+  addUser,
+  getAllUsers,
+  deleteAllUser,
+  userIdCheck,
+  getUser,
+  updateUser,
+  deleteUser,
+};
--- routers/user.router.js
+++ routers/user.router.js
@@ -3,10 +3,18 @@ const {
   addUser,
   getAllUsers,
   deleteAllUser,
+  userIdCheck,
+  getUser,
+  updateUser,
+  deleteUser,
 } = require("../controllers/user.controller");
 
 const userRouter = express.Router();
 
 userRouter.route("/").get(getAllUsers).post(addUser).delete(deleteAllUser);
+
+userRouter.param("userId", userIdCheck);
+
+userRouter.route("/:userId").get(getUser).post(updateUser).delete(deleteUser);
 
 module.exports = { userRouter };
